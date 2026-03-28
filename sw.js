3ac4960ada19803758a99eaf53bf6f058b105b63
João Hummel <joao.vhsousa@gmail.com>
2026-03-28 12:30:44 -0300

    Revert project to 6c1257677961b58e8f42a5edecf6410dc1a09639, preserving public/icones

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-const CACHE = 'nossoap-v6';
+const CACHE = 'nossoap-v3';
 const STATIC_ASSETS = [
   './',
   './index.html',
   './manifest.json',
 ];
 const CDN_ASSETS = [
-  'https://unpkg.com/htm@3.1.1/preact/standalone.umd.js',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
   'https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js',
